35c2af4e2c7146bf29f1b88a9675c4a7d95b8e03
knight <h.knight@gmx.de>
2011-03-27 19:07:18 +0200

    - delete reference - cosmetic changes
    Signed-off-by: knight <h.knight@gmx.de>

--- simu.shrink.A.R
+++ simu.shrink.A.R
@@ -6,11 +6,12 @@ for (i in 1:length(K_)) {
 	for (j in 1:length(p_)) {
 		K	= K_[i]
 		p	= p_[j]
-		cat("Generate data for K = ",K,"with p = ",p," outliers...\n")
+		cat("Generate data for K =",K,"with p =",p,"outliers...\n")
 		# generate the data and evaluate on the fly
 		ed	= sapply(1:num, eval.F.statistic, K, p)
 
 		# save evaluated data to file
 		save(ed, file = paste(dir.output,	"/K",K,"_p",p,".rdata", sep=""))
+		cat("Done!\n")
 	}
 }
